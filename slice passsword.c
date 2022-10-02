84c6dbabe143d1f1262e37feae961815f23c46ff
Jahanzab Jhandeer <74298308+Jahanzab-Jhandeer@users.noreply.github.com>
2022-10-02 02:51:12 -0700

    slice the password function

---
+++ slice passsword.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+  
+  // password slice function
+
+  void slice(char str[] , int n, int m);
+
+int main () {
+  char str[] = "jahanzab";
+  slice(str, 3, 6);
+
+return 0;
+}
+
+void slice (char str[] , int n, int m) {
+   char newstr[100];
+   int j = 0;
+   for(int i=n; i<=m; i++, j++) {
+      newstr[j] = str[i];
+   }
+   newstr[j] = '\0';
+   puts(newstr);
+}
+
